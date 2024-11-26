7abbe30215ffe823b6c93b60abe7d394b8ae268e
mac <mac@macs-MacBook-Pro.local>
2024-11-27 05:00:36 +0530

    redifined tidg database

--- config/database.php
+++ config/database.php
@@ -58,8 +58,8 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'options'   => [
-                // PDO::MYSQL_ATTR_SSL_KEY => env('DB_SSL_KEY'),
-                // PDO::MYSQL_ATTR_SSL_CERT => env('DB_SSL_CERT'),
+                PDO::MYSQL_ATTR_SSL_KEY => env('DB_SSL_KEY'),
+                PDO::MYSQL_ATTR_SSL_CERT => env('DB_SSL_CERT'),
                 PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA')
             ],
         ],
